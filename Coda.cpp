9fa63437b3418ad4e405f077fcb88a64fe28370b
AlessandroSeggi <a.seggi@studenti.uniba.it>
2024-12-06 11:40:40 +0100

    Coda.cpp

--- Coda.cpp
+++ Coda.cpp
@@ -1,15 +1,3 @@
-/*Creazione della classe Coda
-La classe Coda deve rappresentare una struttura dati FIFO (First In, First Out). Utilizzare una implementazione con vettore circolare o con liste puntate (è possibile riutilizzare la classe ListaPuntatori della scorsa lezione).
-
-Implementare i seguenti operatori di base:
-
-enqueue(T elem) – Aggiunge un elemento alla fine della coda.
-dequeue() – Rimuove l’elemento in testa alla coda e lo restituisce. Gestire eventuali errori (ad esempio, coda vuota).
-front() – Restituisce l’elemento in testa alla coda senza rimuoverlo.
-isEmpty() – Restituisce true se la coda è vuota, altrimenti false.
-size() – Restituisce il numero di elementi nella coda.*/
-
-
 #include<iostream>
 #include"ListaPuntatori.cpp"
 
